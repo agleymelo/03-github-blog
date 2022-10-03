ab722d9c04867ca0c762e44f6c4b8cf855038a41
Agleylson Melo <agleylsonmelo@outlook.com>
2022-10-03 19:27:45 -0300

    feat: create component Header

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,8 @@
 import styled from 'styled-components'
 
-export const HeaderContainer = styled.header``
+export const HeaderContainer = styled.header`
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`
---
+++ src/layout/DefaultLayout/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const DefaultLayoutContainer = styled.div`
+  .container {
+    width: 100%;
+    max-width: 70rem;
+
+    margin-right: auto;
+    margin-left: auto;
+  }
+`
